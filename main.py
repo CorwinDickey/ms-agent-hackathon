73bd96926446c0d1bb5be9bef96c29864d455a8f
Corwin Dickey <62188881+CorwinDickey@users.noreply.github.com>
2025-04-12 22:06:51 -0500

    ollama setup now working with proper pulling of model

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import logging
+import requests
 from tkinter.filedialog import askopenfilename
 from agents.python_agent import PythonAgent
+from sdk.utils.global_variables import DEFAULT_MODEL_NAME
 from sdk.utils.docker_utils import compose_up, copy_file_to_container
 from sdk.utils.logger import get_logger
 
@@ -9,3 +11,10 @@ app_logger = get_logger("tdd.aiLogger", level=logging.INFO)
 print("Composing docker...")
 compose_up(app_logger)
 print("Docker environment ready.")
+
+print("Setting up Ollama...")
+url = "http://localhost:11434/api/pull"
+data = {"model":DEFAULT_MODEL_NAME}
+headers = {"content-type":"application/json"}
+response = requests.post(url, json=data, headers=headers)
+print("Finished setting up Ollama.")
